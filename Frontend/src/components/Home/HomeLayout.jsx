7928af71a08d2863c1621da990410f9e4d3dd664
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2024-10-12 00:53:53 +0530

    HomePage files created

---
+++ Frontend/src/components/Home/HomeLayout.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const HomeLayout = () => {
+  return <div></div>;
+};
+
+export default HomeLayout;
---
+++ Frontend/src/components/Home/components/userProfile.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const userProfile = () => {
+  return <div>UserProfile</div>;
+};
+
+export default userProfile;
