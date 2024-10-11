1de5d612b56061a3e732254ed6f57d648b3be800
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2024-10-11 23:55:44 +0530

    swap section logic complete

--- Frontend/src/components/authentication/AuthLayout.jsx
+++ Frontend/src/components/authentication/AuthLayout.jsx
@@ -1,14 +1,59 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import { IoIosArrowDropdown } from "react-icons/io";
+import { motion } from "framer-motion";
 
 const AuthLayout = () => {
+  const [signupActive, setSignupActive] = useState(false);
+
+  const scrollToSignup = () => {
+    const signupSection = document.getElementById("signupSection");
+    signupSection.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const scrollToLogin = () => {
+    const loginSection = document.getElementById("loginSection");
+    loginSection.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const handleToggle = () => {
+    if (signupActive) {
+      scrollToLogin();
+    } else {
+      scrollToSignup();
+    }
+    setSignupActive(!signupActive);
+  };
+
+  useEffect(() => {
+    scrollToLogin();
+  }, []);
   return (
     <div>
-      <Login />
-      <button>Scroll</button>
-      <Signup />
+      <div id="loginSection" className="min-h-screen">
+        <Login />
+      </div>
+
+      <div className="flex justify-center">
+        <motion.button
+          initial={{ rotate: 180 }}
+          animate={{
+            y: signupActive ? 180 : -120,
+            rotate: signupActive ? 180 : 0,
+          }}
+          whileHover={{ scale: 1.2 }}
+          whileTap={{ scale: 0.9 }}
+          onClick={handleToggle}
+          className=" absolute bg-white text-primary text-5xl p-1 rounded-full"
+        >
+          <IoIosArrowDropdown />
+        </motion.button>
+      </div>
+      <div id="signupSection" className="min-h-screen">
+        <Signup />
+      </div>
     </div>
   );
 };
